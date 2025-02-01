88b964fbb4f5a858d9f2c23b07ed62f2355952cd
Prince Kumar <iprince0302@gmail.com>
2025-02-01 14:17:56 +0530

    basic of cpp

---
+++ basic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // this is comment
+    int a, b;
+    cout<<"enter the value of a and b"<<endl;
+    cin>>a;
+    cout<<endl;
+    cin>>b;
+    cout<<endl<<"the value of a is "<<a<< "and the value of b is "<<b;
+
+
+}
